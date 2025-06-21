2a0e78cae556d02c66dc2c44233f78bb72ef5718
MateusCunha-Dev <mateuscunhadev@gmail.com>
2025-06-20 21:08:14 -0300

    Implementação da entidade TipoConta e criacção da entidade Historico

---
+++ src/main/java/com/cefet/sgr_backend/entities/Historico.java
@@ -0,0 +1,5 @@
+package com.cefet.sgr_backend.entities;
+
+public class Historico {
+
+}
--- src/main/java/com/cefet/sgr_backend/entities/TipoConta.java
+++ src/main/java/com/cefet/sgr_backend/entities/TipoConta.java
@@ -1,5 +1,56 @@
 package com.cefet.sgr_backend.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbTipo")
 public class TipoConta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idTipo") 
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String nome;
+
+    public TipoConta() {}
+
+    public TipoConta(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TipoConta)) return false;
+        TipoConta tipoConta = (TipoConta) o;
+        return Objects.equals(id, tipoConta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
