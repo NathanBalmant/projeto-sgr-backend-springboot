d01b14ba215f4ec0f7b96177dc598177986046fd
Samuel Oliveira <samuferraz12052004@gmail.com>
2025-06-22 15:34:11 -0300

    Criação do DTO da entidade Morador

--- src/main/java/com/cefet/sgr_backend/dto/MoradorDTO.java
+++ src/main/java/com/cefet/sgr_backend/dto/MoradorDTO.java
@@ -1,5 +1,59 @@
 package com.cefet.sgr_backend.dto;
 
+import com.cefet.sgr_backend.entities.Morador;
+
 public class MoradorDTO {
 
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String celular;
+    private String email;
+    private String contatoFamilia;
+    private String foto;
+
+    
+    public MoradorDTO() {
+    }
+
+    public MoradorDTO(Morador morador){
+        this.nome = morador.getNome();
+        this.cpf = morador.getCpf();
+        this.dataNascimento = morador.getDataNascimento().toString();
+        this.celular = morador.getCelular();
+        this.email = morador.getEmail();
+        this.contatoFamilia = morador.getContatoFamilia();
+        this.foto = morador.getFoto();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getContatoFamilia() {
+        return contatoFamilia;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+
+    
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha) VALUES ('Samuel', '11993347666', '2004-05-12', '3196903134', 'samuferraz12052004@gmail.com', 'Mãe: 31988994231', 'samuel', '1234');
+INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha, foto) VALUES ('Samuel', '11993347666', '2004-05-12', '3196903134', 'samuferraz12052004@gmail.com', 'Mãe: 31988994231', 'samuel', '1234', '.');
 
-INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha) VALUES ('Ana', '98765432100', '2000-08-14', '31988887777', 'ana@gmail.com', 'Pai: 31977778888', 'ana', 'abcd');
+INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha, foto) VALUES ('Ana', '98765432100', '2000-08-14', '31988887777', 'ana@gmail.com', 'Pai: 31977778888', 'ana', 'abcd', '*');
 
-INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha) VALUES ('João', '11122233344', '1999-09-16', '31987654321', 'joao@gmail.com', 'Tia: 31966665555', 'joao', 'senha');
+INSERT INTO tb_morador (nome, cpf, data_nascimento, celular, email, contato_familia, login, senha, foto) VALUES ('João', '11122233344', '1999-09-16', '31987654321', 'joao@gmail.com', 'Tia: 31966665555', 'joao', 'senha', '^');
