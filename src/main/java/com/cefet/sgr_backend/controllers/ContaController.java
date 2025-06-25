9dd3913b28432ffbb69edaf942fdf92a7d5be759
Samuel Oliveira <samuferraz12052004@gmail.com>
2025-06-25 19:31:17 -0300

    Criação da classe ContaController

---
+++ src/main/java/com/cefet/sgr_backend/controllers/ContaController.java
@@ -0,0 +1,48 @@
+package com.cefet.sgr_backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.cefet.sgr_backend.dto.ContaDTO;
+import com.cefet.sgr_backend.services.ContaService;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+
+    @GetMapping
+    public ResponseEntity<List<ContaDTO>> findAll() {
+        List<ContaDTO> lista = contaService.findAll();
+        return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContaDTO> findById(@PathVariable Long id) {
+        ContaDTO dto = contaService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ContaDTO> insert(@RequestBody ContaDTO dto) {
+        ContaDTO nova = contaService.insert(dto);
+        return ResponseEntity.status(201).body(nova);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ContaDTO> update(@PathVariable Long id, @RequestBody ContaDTO dto) {
+        ContaDTO atualizada = contaService.update(id, dto);
+        return ResponseEntity.ok(atualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        contaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
